662b55ddcfe4357af0b1bd9ceb74bf024028fe37
Nguyen_Xuan_Cuong <chrollolucifer97@gmail.com>
2023-10-11 23:50:28 +0700

    Bài 16.2

--- Buổi_7/Bài 16_2/js/main.js
+++ Buổi_7/Bài 16_2/js/main.js
@@ -10,21 +10,19 @@ $(document).ready(function() {
 
     function toggleSubmenu() {
         if ($(window).width() <= 767) {
-            // If the screen width is 767px or smaller, enable click behavior on "Blog"
+
             $(".header__menu-list li.hide > a").click(function(e) {
                 e.preventDefault();
                 $(this).siblings('.submenu').slideToggle();
                 $(this).find('i').toggleClass("fa-angle-down fa-angle-right");
             });
         } else {
-            // If the screen width is larger than 767px, disable click behavior on "Blog"
+
             $(".header__menu-list li.hide > a").off("click");
         }
     }
 
-    // Initially, set up the behavior according to the current screen width
     toggleSubmenu();
 
-    // Update the behavior when the window is resized
     $(window).resize(toggleSubmenu);
 });
