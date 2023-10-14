735aebd5e4b2d591c54fceef4f2f3afa85973197
Nguyen_Xuan_Cuong <chrollolucifer97@gmail.com>
2023-10-14 10:54:02 +0700

    Buổi 8

--- Buổi_8/Bài 19_1/js/main.js
+++ Buổi_8/Bài 19_1/js/main.js
@@ -1,4 +1,4 @@
-//  Window scroll sticky class add
+//  Phần scroll 
 function windowScroll() {
     const navbar = document.getElementById("navbar");
     if (navbar) {
@@ -18,28 +18,3 @@ window.addEventListener('scroll', (ev) => {
     windowScroll();
 })
 
-
-// number counter
-const counter = document.querySelectorAll('.counter_value');
-const speed = 250; // The lower the slower
-
-counter.forEach(counter_value => {
-	const updateCount = () => {
-		const target = +counter_value.getAttribute('data-target');
-		const count = +counter_value.innerText;
-
-		const inc = target / speed;
-
-		// Check if target is reached
-		if (count < target) {
-            // Add inc to count and output in counter_value
-			counter_value.innerText = (count + inc).toFixed(0);
-			// Call function every ms
-			setTimeout(updateCount, 1);
-		} else {
-			counter_value.innerText = target;
-		}
-	};
-
-	updateCount();
-});
