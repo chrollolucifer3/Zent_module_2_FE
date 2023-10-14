1e1c41d895e0928f20bff4b706a36676af8a1090
Nguyen_Xuan_Cuong <chrollolucifer97@gmail.com>
2023-10-15 02:33:20 +0700

    Buổi 8

--- Buổi_8/Bài 19_1/js/jquery.js
+++ Buổi_8/Bài 19_1/js/jquery.js
@@ -1,20 +1,14 @@
 $(document).ready(function() {
     $('#submitBtn').click(function(event) {
-        event.preventDefault(); // Ngăn chặn gửi form mặc định
+        event.preventDefault();
 
-        // Lấy giá trị từ trường email
         var email = $('#emailInput').val();
 
-        // Kiểm tra email hợp lệ bằng biểu thức chính quy
         var emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
         if (!email.match(emailRegex)) {
-            // Hiển thị thông báo lỗi dưới trường email
             $('#emailError').text('Email không hợp lệ');
         } else {
-            // Xóa thông báo lỗi nếu email hợp lệ
             $('#emailError').text('');
-            
-            // Email hợp lệ, thực hiện xử lý gửi form ở đây
         }
     });
 });
